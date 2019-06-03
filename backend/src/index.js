ea9a12a0955808cbc53d2f7eec8ffe97d3ac8cb7
Pedro Ferrari <pedro@pedroferrari.com>
2019-06-03 15:51:08 -0700

    Initial Commit EP 14

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config({ path: 'variables.env'})
+const createServer = require('./createServer')
+const db = require('./db')
+
+const server = createServer()
+
+// TODO Use express middleware to handle cookies (JsonWebToken:JWT)
+// TODO Use express middleware to populate current user
+
+server.start({
+  cors: {
+    crendentials: true,
+    origin: process.env.FRONTEND_URL
+  }
+}, deets => {
+    console.log(`Server is now running on http://localhost:${deets.port}`)
+  }
+)
